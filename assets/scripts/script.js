3dbc3edfc470783a817975f9d1aeaa0c9558aad3
JB <bradburyjesse7@gmail.com>
2023-11-20 22:44:10 -0700

    Added basic outline of project

---
+++ assets/scripts/script.js
@@ -0,0 +1,16 @@
+// Psuedo code for this project. 
+
+// We need to call in the Weather API we will be using
+
+// We need a headder. 
+
+// We need the location entry box. 
+// When the user searches a location, we will pull data from the weather API about that location. 
+// We will need some system of getting the long/lat of the location to place into the API 
+
+// We need a spot that displays the previous search location that the user has searched. 
+
+// We need an empty div that will display thecurrent conditions at the top and the 5 day forcast at the bottom. 
+
+// When the user submits a new location, or clicks a previously searched one, the 5 day forecast is displayed in the empty div. 
+
