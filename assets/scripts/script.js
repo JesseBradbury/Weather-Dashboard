b99a50e70dc7c866f78ec2f0945d471d1568a1fa
JB <bradburyjesse7@gmail.com>
2023-11-27 16:53:41 -0700

    Added user input and serach funstions.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -5,9 +5,9 @@
 // This code will get the weather data for the location inputed by the user. 
 var weatherReport = document.getElementById("weather-report")
 
-function getTestApi() {
-    var lat = "33.448376";
-    var lon = "-112.074036";
+function getTestApi(lat,lon) {
+    // var lat = "33.448376";
+    // var lon = "-112.074036";
     var units = "imperial"
     var apiUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&units=' + units + '&appid=7683dd89e3713c696366aefeb8fa991f';
 
@@ -17,11 +17,12 @@ function getTestApi() {
         })
         .then(function (data) {
             var listName = document.createElement("h2");
-            var iconCode = data.list[0].weather[0].icon; // Accessing the icon code from the API response
-            var iconUrl = 'https://openweathermap.org/img/w/' + iconCode + '.png'; // Constructing the icon URL
+            
+            var iconCode = data.list[0].weather[0].icon; 
+            var iconUrl = 'https://openweathermap.org/img/w/' + iconCode + '.png';
             var iconImg = document.createElement("img");
             iconImg.src = iconUrl;
-            iconImg.alt = "Weather Icon"; // Add alt text for accessibility
+            iconImg.alt = "Weather Icon";
             
             listName.textContent = data.city.name + " " + "(" + dayjs().format('M/D/YYYY') + ")";
             
@@ -41,42 +42,53 @@ function getTestApi() {
             weatherReport.appendChild(listHumid);
         })
 };
-getTestApi();
+// getTestApi();
 
-
-// var getWeatherData = function (repo) {
-//     var lat = "33.448376";
-//     var lon = "-112.074036";
-//     var apiUrl = 'api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=7683dd89e3713c696366aefeb8fa991f';
-
-//     fetch(apiUrl).then(function (response) {
-//         //   if (response.ok) {
-//         //     response.json().then(function (data) {
-//         //       displayIssues(data);
-
-//         //       if (response.headers.get('Link')) {
-//         //         displayWarning(repo);
-//         //       }
-//         //     });
-//         //   } else {
-//         //     document.location.replace('./index.html');
-//         //   }
-//     });
-// };
-
-var locationGeoData = function () {
-    var geoApiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=1&appid=7683dd89e3713c696366aefeb8fa991f"
-
-    fetch(geoApiUrl)
-        .then(function (response) {
-            return response.json()
-        })
-}
-
-// We need a header. 
 
 // We need the location entry box. 
+var locationSearchEl = document.querySelector("#search-box");
+
 // When the user searches a location, we will pull data from the weather API about that location. 
+
+
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    
+    var locationSearch = locationSearchEl.value.trim();
+    
+    if (locationSearch) {
+        // This is the code to convert the location name into lon and lat values. 
+        var geoApiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + locationSearch + "&limit=1&appid=7683dd89e3713c696366aefeb8fa991f"
+        
+        fetch(geoApiUrl)
+            .then(function (response) {
+                return response.json()
+            })
+            .then(function(data) {
+                if (data.length > 0) {
+                    var lat = data[0].lat;
+                    var lon = data[0].lon;
+                    console.log("Latitude:", lat, "Longitude:", lon);
+                    getTestApi(lat,lon);
+                }
+                else {
+                    console.log("location not found");
+                }
+            })
+            .catch(function(error) {
+                console.log("Error fetching data: ", error);
+            });
+        
+        
+    } else {
+        alert('Please try a different location');
+    }
+};
+
+var form = document.querySelector('form'); 
+form.addEventListener('submit', formSubmitHandler);
+
 
 
 // We will need some system of getting the long/lat of the location to place into the API 
