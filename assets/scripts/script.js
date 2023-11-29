bc27d36ef5e88f9ad61497ac4e53c4c23c0ba305
JB <bradburyjesse7@gmail.com>
2023-11-28 23:48:40 -0700

    Added button logic

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -15,6 +15,7 @@ function storeSearch(location) {
     storedLocations.push(location);
 
     localStorage.setItem("storedLocations", JSON.stringify(storedLocations));
+    displaySearchHistory();
     // Add code that will save the value of the searched location to loacl storage.
 
     // Appends that title to the prev-results-btn text. 
@@ -109,26 +110,56 @@ form.addEventListener('submit', formSubmitHandler);
 // We need a spot that displays the previous search location that the user has searched. 
 // This will be the code to create the previous results buttons. 
 
-var previousSearch = JSON.parse(localStorage.getItem("storedLocations"));
-var searchHistory = document.querySelector("#search-history")
 
 function displaySearchHistory() {
+    var previousSearch = JSON.parse(localStorage.getItem("storedLocations"));
+    var searchHistory = document.querySelector("#search-history")
+
+    searchHistory.innerHTML = "";
+
+    var endIndex = previousSearch.length;
     var startIndex = Math.max(0, previousSearch.length - 10);
+    var uniqueCities = new Set();
 
-    for (var i = startIndex; i < previousSearch.length; i++) {
+    for (var i = endIndex -1; i >= startIndex; i--) {
         var cityName = previousSearch[i];
-        var historyBtn = document.createElement("button");
-        historyBtn.classList.add("prev-results-btn")
-        historyBtn.value = cityName;
-        historyBtn.textContent = cityName;
-        
-        searchHistory.appendChild(historyBtn);
+        if (cityName && !uniqueCities.has(cityName)) {
 
+            var historyBtn = document.createElement("button");
+            historyBtn.classList.add("prev-results-btn")
+            historyBtn.value = cityName;
+            historyBtn.textContent = cityName;
+            attachHoverEffect(historyBtn);
+    
+    
+            
+            searchHistory.appendChild(historyBtn);
+            uniqueCities.add(cityName);
+        }
+        
         // add event listener to each button.
     }
     console.log(cityName);
 }
 displaySearchHistory();
+function attachHoverEffect(button) {
+
+    button.addEventListener("mouseover", function () {
+        button.style.boxShadow = "0 8px 13px 0 rgba(0,0,0,0.24), 0 17px 50px 0 rgba(0,0,0,0.19)";
+    })
+    button.addEventListener("mouseout", function () {
+        button.style.boxShadow = "";
+    });
+}
+
+
+
+var resetPress = function resetSearchHistory() {
+    localStorage.removeItem("storedLocations");
+    window.location.reload();
+};
+var clearPress = document.querySelector("#clear");
+clearPress.addEventListener('click', resetPress);
 
 
 // We need an empty div that will display thecurrent conditions at the top and the 5 day forcast at the bottom. 
