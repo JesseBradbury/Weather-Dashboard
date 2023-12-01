b5cb88bc94fd6bd4d10fd8fa99f279be544fe972
JB <bradburyjesse7@gmail.com>
2023-12-01 11:17:40 -0700

    Formatting, adding comments

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,25 +1,13 @@
-// Psuedo code for this project. 
 
-// We need to call in the Weather API we will be using
-
-// This code will get the weather data for the location inputed by the user. 
 var weatherReport = document.getElementById("weather-report");
 var forecast = document.getElementById("forecast");
 
-
+// Function that clears the Weather report when a user selects a new location
 function clearWeatherReport() {
     weatherReport.innerHTML = "";
 }
 
-// function clearForecastHead() {
-//     forecastHead.innerHTML = "";
-// }
-
-// function clearForecastCards() {
-//     forecastCards.innerHTML = "";
-
-// }
-
+// Function that stores the searched locations by the user. 
 function storeSearch(location) {
     var storedLocations = JSON.parse(localStorage.getItem("storedLocations")) || [];
 
@@ -27,12 +15,11 @@ function storeSearch(location) {
 
     localStorage.setItem("storedLocations", JSON.stringify(storedLocations));
     displaySearchHistory();
-    // Add code that will save the value of the searched location to loacl storage.
-
-    // Appends that title to the prev-results-btn text. 
 
 }
 
+// This is the function that gets the Weather Data from the openWeather API. It takes in the Lat and Lon from the storeSearch function, using the geoAPI from openWeather. 
+// It then creates the Weather Report section from that data. 
 function getWeatherApi(lat, lon) {
     var units = "imperial"
     var apiUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&units=' + units + '&appid=7683dd89e3713c696366aefeb8fa991f';
@@ -42,17 +29,19 @@ function getWeatherApi(lat, lon) {
             return response.json();
         })
         .then(function (data) {
-            var listName = document.createElement("h2");
 
+            // Code to display the name and Date. Uses dayJS to help format the date from the data. 
+            var listName = document.createElement("h2");
+            listName.textContent = data.city.name + " " + "(" + dayjs().format('M/D/YYYY') + ")";
+
+            // Code that displays the weather icon to the weather report section
             var iconCode = data.list[0].weather[0].icon;
             var iconUrl = 'https://openweathermap.org/img/w/' + iconCode + '.png';
             var iconImg = document.createElement("img");
             iconImg.src = iconUrl;
             iconImg.alt = "Weather Icon";
 
-            // listName.textContent = locationSearchEl.value.trim() + " " + "(" + dayjs().format('M/D/YYYY') + ")";
-            listName.textContent = data.city.name + " " + "(" + dayjs().format('M/D/YYYY') + ")";
-
+            // Code that displays the temp, wind and humid to the weather report section
             var listTemp = document.createElement("p");
             listTemp.textContent = "Temp: " + data.list[0].main.temp;
 
@@ -68,7 +57,7 @@ function getWeatherApi(lat, lon) {
             weatherReport.appendChild(listWind);
             weatherReport.appendChild(listHumid);
 
-            // document.addEventListener("DOMContentLoaded", function () {
+            // This is the code that creates the 5 day forecast section
             var forecastHead = document.getElementById("forecast-head")
 
             function clearForecastHead() {
@@ -86,71 +75,58 @@ function getWeatherApi(lat, lon) {
             }
             clearForecastCards();
 
-            // for (let i = 5; i < 40; i+8) {
-                for (let i = 5; i <= 37; i += 8) {
-                    
-                
-                
+            // This fore loop creates the  cards. 
+            for (let i = 5; i <= 37; i += 8) {
+
+
+
                 var dayCard = document.createElement("section");
                 dayCard.classList.add("day-card");
-                
-                
-                var cardFormatedDate = "(" + data.list[i].dt_txt.slice(5,7) + "/" + data.list[i].dt_txt.slice(8,10) + "/" + data.list[i].dt_txt.slice(0,4) + ")";
-                
+
+
+                var cardFormatedDate = "(" + data.list[i].dt_txt.slice(5, 7) + "/" + data.list[i].dt_txt.slice(8, 10) + "/" + data.list[i].dt_txt.slice(0, 4) + ")";
+
                 var cardDate = document.createElement("h4");
                 cardDate.textContent = cardFormatedDate;
-                
+
                 var cardIconCode = data.list[i].weather[0].icon;
                 var cardIconUrl = 'https://openweathermap.org/img/w/' + cardIconCode + '.png';
                 var cardIconImg = document.createElement("img");
                 cardIconImg.src = cardIconUrl;
                 cardIconImg.alt = "Weather Icon";
-                
+
                 var cardListTemp = document.createElement("p");
                 cardListTemp.textContent = "Temp: " + data.list[i].main.temp;
-                
+
                 var cardListWind = document.createElement("p");
                 cardListWind.textContent = "Wind: " + data.list[i].wind.speed + " MPH";
-                
+
                 var cardListHumid = document.createElement("p");
                 cardListHumid.textContent = "Humidity: " + data.list[i].main.humidity + " %";
-                
+
                 dayCard.appendChild(cardDate);
                 dayCard.appendChild(cardIconImg);
                 dayCard.appendChild(cardListTemp);
                 dayCard.appendChild(cardListWind);
                 dayCard.appendChild(cardListHumid);
-                
-                
+
+
                 forecastCards.appendChild(dayCard);
             }
-            // });
-            // var 
+
 
 
             console.log(data)
         })
 };
 
-var createFiveDay = function () {
 
-    var forecastHead = document.getElementById("forecast-head")
-    var fiveDayForecast = document.createElement("h3");
-    fiveDayForecast.textContent = "5-Day Forecast:";
-    forecastHead.append(fiveDayForecast);
+// This is the code that runs the search functionality and the search history.
 
-    // TODO: Create cards for 5 day forecast. 
-    var forecastCards = document.getElementById("forecast-cards");
-    var dayCard = document.createElement("section")
-    dayCard.classList.add("day-card")
-
-    forecastCards.appendChild(dayCard);
-}
-
-// We need the location entry box. 
 var locationSearchEl = document.querySelector("#search-box");
 
-// When the user searches a location, we will pull data from the weather API about that location. 
+// When the user searches a location, we will pull data from the weather API about that locations Lat and Lon, so we can run it through the openWeather API. 
+// It also saves this location to local storage.  
 
 var formSubmitHandler = function (event) {
     event.preventDefault();
